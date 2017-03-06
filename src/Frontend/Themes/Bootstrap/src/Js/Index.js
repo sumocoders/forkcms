52a74bddb10a01f150b13f7cd6e67aed8fa42a36
Katrien Vanhaute <vanhautekatrien@gmail.com>
2017-03-06 14:29:28 +0100

    Start with a capital for Js folders

--- src/Frontend/Themes/Bootstrap/src/Js/Index.js
+++ src/Frontend/Themes/Bootstrap/src/Js/Index.js
@@ -3,8 +3,8 @@ import Bootstrap from 'bootstrap-sass/assets/javascripts/bootstrap';
 
 /* Utilities imports */
 import SweetScroll from "sweet-scroll";
-import ResizeFunction from './utilities/Resize';
-/*import {Fancybox} from './utilities/Fancybox';*/
+import ResizeFunction from './Utilities/Resize';
+/*import {Fancybox} from './Utilities/Fancybox';*/
 
 /* Theme imports */
 /* eg. import tooltip from './Theme/Tooltip'; */
