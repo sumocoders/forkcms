4190616d646c79f76f7469c7519740be126ca613
Mathias Helin <mathias@sumocoders.be>
2017-08-03 11:33:03 +0200

    Apply Standard JS code styling to our old Bootstrap theme

--- src/Frontend/Themes/Bootstrap/src/Js/Utilities/Resize.js
+++ src/Frontend/Themes/Bootstrap/src/Js/Utilities/Resize.js
@@ -1,32 +1,32 @@
-import requestAnimationFrame from 'requestanimationframe';
-import DistributeHeight from './DistributeHeight';
+import requestAnimationFrame from 'requestanimationframe'
+import DistributeHeight from './DistributeHeight'
 
 export default class {
 
-    constructor() {
-        this.resize();
+  constructor () {
+    this.resize()
+  }
+
+  resize () {
+    let calculate,
+      tick,
+      ticking = false
+
+    calculate = function () {
+      new DistributeHeight()
+      ticking = false
     }
 
-    resize() {
-        let calculate,
-            tick,
-            ticking = false;
-
-        calculate = function() {
-            new DistributeHeight();
-            ticking = false;
-        };
-
-        tick = function() {
-            if (!ticking) {
-                requestAnimationFrame(calculate);
-                ticking = true;
-            }
-        };
-        tick();
-
-        $(window).on('load resize', function() {
-            tick();
-        });
+    tick = function () {
+      if (!ticking) {
+        requestAnimationFrame(calculate)
+        ticking = true
+      }
     }
+    tick()
+
+    $(window).on('load resize', function () {
+      tick()
+    })
+  }
 }
