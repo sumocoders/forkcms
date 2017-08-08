e4246a59ee60f0ddb802f6292e98e252e759e83e
Mathias Helin <mathias@sumocoders.be>
2017-08-08 17:21:55 +0200

    Remove old deployment config files

--- deployment/staging.rb
+++
@@ -1,5 +0,0 @@
-set :branch, "staging"
-set :staging_server, "dev02.sumocoders.eu"
-set :staging_url, "#{project}.#{client}.php71.sumocoders.eu"
-set :document_root, "/home/#{user}/php71/#{client}/#{project}"
-set :url, "#{staging_url}"
