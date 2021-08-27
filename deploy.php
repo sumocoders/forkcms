8fbae166655f6a5ba20204416f87ef2cbc854410
Davy De Vuysdere <davy@sumocoders.be>
2021-08-27 12:56:00 +0200

    create symlink

--- deploy.php
+++ deploy.php
@@ -126,6 +126,54 @@ task('database:migrate', function () {
     ->addBefore('build:assets:npm');
 after('deploy:update_code', 'upload:assets');*/
 
+task(
+    'sumo:symlink:document-root',
+    function() {
+        if (!get('document_root', false)) {
+            return;
+        }
+
+        $publicPath = get('deploy_path') . '/current/';
+        $currentSymlink = run(
+            'if [ -L {{document_root}} ]; then readlink {{document_root}}; fi'
+        );
+
+        $expandedPath = str_replace(
+            '~/',
+            run('echo $HOME') . '/',
+            $publicPath
+        );
+
+        // already linked, so we can stop here
+        if ($currentSymlink === $expandedPath) {
+            return;
+        }
+
+        // Show a warning when the document root exists. So we don't overwrite
+        // existing stuff.
+        if ($currentSymlink === '' && test('[ -e {{document_root}} ]')) {
+            writeln(
+                [
+                    '<comment>Document root already exists</comment>',
+                    '<comment>To link it, issue the following command:</comment>',
+                    sprintf(
+                        '<comment>ln -sf %1$s %2$s</comment>',
+                        $publicPath,
+                        get('document_root')
+                    ),
+                ]
+            );
+            return;
+        }
+
+        run(sprintf('mkdir -p %1$s', dirname(get('document_root'))));
+        run('rm -f {{document_root}}');
+        run(sprintf('{{bin/symlink}} %1$s {{document_root}}', $publicPath));
+    }
+)
+    ->desc('Symlink the document root to the public folder');
+after('deploy:symlink', 'sumo:symlink:document-root');
+
 /**********************
  * Flow configuration *
  **********************/
