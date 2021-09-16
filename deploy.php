27d5a5d58efb818b42e8dbebd8acbae96b7b8351
Davy De Vuysdere <davy@sumocoders.be>
2021-09-16 11:52:39 +0200

    remove sumo:symlink:document-root from deploy.php and use the one from deployer-sumo

--- deploy.php
+++ deploy.php
@@ -337,54 +337,6 @@ task(
     ->desc('Clear Fork CMS cache');
 before('deploy:cache:clear', 'fork:cache:clear');
 
-task(
-    'sumo:symlink:document-root',
-    function() {
-        if (!get('document_root', false)) {
-            return;
-        }
-
-        $publicPath = get('deploy_path') . '/current/';
-        $currentSymlink = run(
-            'if [ -L {{document_root}} ]; then readlink {{document_root}}; fi'
-        );
-
-        $expandedPath = str_replace(
-            '~/',
-            run('echo $HOME') . '/',
-            $publicPath
-        );
-
-        // already linked, so we can stop here
-        if ($currentSymlink === $expandedPath) {
-            return;
-        }
-
-        // Show a warning when the document root exists. So we don't overwrite
-        // existing stuff.
-        if ($currentSymlink === '' && test('[ -e {{document_root}} ]')) {
-            writeln(
-                [
-                    '<comment>Document root already exists</comment>',
-                    '<comment>To link it, issue the following command:</comment>',
-                    sprintf(
-                        '<comment>ln -sf %1$s %2$s</comment>',
-                        $publicPath,
-                        get('document_root')
-                    ),
-                ]
-            );
-            return;
-        }
-
-        run(sprintf('mkdir -p %1$s', dirname(get('document_root'))));
-        run('rm -f {{document_root}}');
-        run(sprintf('{{bin/symlink}} %1$s {{document_root}}', $publicPath));
-    }
-)
-    ->desc('Symlink the document root to the public folder');
-after('deploy:symlink', 'sumo:symlink:document-root');
-
 desc('Replace the local files with the remote files');
 task(
     'sumo:files:get',
