93eb53f6ee57d53905b5e4e67031612fcd446b90
Daphne Slootmans <daphne@sumocoders.be>
2023-07-06 11:16:40 +0200

    Refactor sumocoders theme gulp tasks to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,75 @@
-var webpack = require('webpack')
+const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require("copy-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV === 'development'
 
 module.exports = {
-  output: {
-    filename: 'bundle.js'
+  entry: {
+    screen: ['./src/Frontend/Themes/Bootstrap/src/Js/Index.js', './src/Frontend/Themes/Bootstrap/src/Layout/Sass/screen.scss']
   },
-  resolve: {
-    modules: ['node_modules']
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'src/Frontend/Themes/Bootstrap/Core'),
+    clean: true,
+  },
+  mode: "development",
+  externals: ['jquery'],
+  watchOptions: {
+    aggregateTimeout: 600,
+    ignored: /node_modules/,
   },
   plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      Popper: ['popper.js', 'default']
-    })
+    new MiniCssExtractPlugin({
+      filename: './Layout/Css/[name].css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "./Layout/Templates/**/*",
+          context: path.resolve(__dirname, "src", "Frontend", "Themes", "Bootstrap", "src"),
+        },
+        {
+          from: "./Layout/Images/**/*",
+          context: path.resolve(__dirname, "src", "Frontend", "Themes", "Bootstrap", "src"),
+        }
+      ],
+    }),
   ],
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$|jsx/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          cacheDirectory: true,
-          presets: ['@babel/preset-env']
-        }
+        exclude: /node_modules/
       },
       {
         // separate loader for Bootstrap because it needs to be compiled
         test: /bootstrap\.js$/,
-        loader: 'babel-loader',
-        query: {
-          cacheDirectory: true,
-          presets: ['@babel/preset-env']
-        }
-      }
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|svg|eot)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: './Layout/Fonts/[name][ext]',
+        },
+      },
     ]
   }
 }
