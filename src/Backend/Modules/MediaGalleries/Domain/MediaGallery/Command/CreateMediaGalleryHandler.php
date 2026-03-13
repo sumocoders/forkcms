bf4d511773d3fe83ef08bb3902cb520167de4503
Jonas De Keukelaere <jonas@sumocoders.be>
2026-03-13 10:09:10 +0000

    Convert media gallery crud with messenger

--- src/Backend/Modules/MediaGalleries/Domain/MediaGallery/Command/CreateMediaGalleryHandler.php
+++ src/Backend/Modules/MediaGalleries/Domain/MediaGallery/Command/CreateMediaGalleryHandler.php
@@ -4,20 +4,26 @@ namespace Backend\Modules\MediaGalleries\Domain\MediaGallery\Command;
 
 use Backend\Modules\MediaGalleries\Domain\MediaGallery\MediaGallery;
 use Backend\Modules\MediaGalleries\Domain\MediaGallery\MediaGalleryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
+#[AsMessageHandler]
 final readonly class CreateMediaGalleryHandler
 {
-    public function __construct(private MediaGalleryRepository $mediaGalleryRepository)
-    {
+    public function __construct(
+        private MediaGalleryRepository $mediaGalleryRepository,
+        private EntityManagerInterface $entityManager,
+    ) {
     }
 
-    public function handle(CreateMediaGallery $createMediaGallery): void
+    public function __invoke(CreateMediaGallery $createMediaGallery): void
     {
-        /** @var MediaGallery $mediaGallery */
         $mediaGallery = MediaGallery::fromDataTransferObject($createMediaGallery);
         $this->mediaGalleryRepository->add($mediaGallery);
 
         // We redefine the mediaGallery, so we can use it in an action
         $createMediaGallery->setMediaGalleryEntity($mediaGallery);
+
+        $this->entityManager->flush();
     }
 }
--- src/Backend/Modules/MediaGalleries/Domain/MediaGallery/Command/UpdateMediaGalleryHandler.php
+++ src/Backend/Modules/MediaGalleries/Domain/MediaGallery/Command/UpdateMediaGalleryHandler.php
@@ -3,12 +3,22 @@
 namespace Backend\Modules\MediaGalleries\Domain\MediaGallery\Command;
 
 use Backend\Modules\MediaGalleries\Domain\MediaGallery\MediaGallery;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
-final class UpdateMediaGalleryHandler
+#[AsMessageHandler]
+final readonly class UpdateMediaGalleryHandler
 {
-    public function handle(UpdateMediaGallery $updateMediaGallery): void
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+    ) {
+    }
+
+    public function __invoke(UpdateMediaGallery $updateMediaGallery): void
     {
         // We redefine the mediaGallery, so we can use it in an action
         $updateMediaGallery->setMediaGalleryEntity(MediaGallery::fromDataTransferObject($updateMediaGallery));
+
+        $this->entityManager->flush();
     }
 }
